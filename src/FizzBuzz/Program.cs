52c1d51fe09700e9ba6beaeca9a9f6214ff35af9
Andrew Darley <Andrew.Darley@connectib.com>
2017-05-13 13:21:08 +0100

    Excluded console app from coverage. Added NUnit test adapter

--- src/FizzBuzz/Program.cs
+++ src/FizzBuzz/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace FizzBuzz
 {
+    [ExcludeFromCodeCoverage]
     class Program
     {
         static void Main(string[] args)
         {
             Console.ReadKey();
         }
-
     }
 }
